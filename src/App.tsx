d73b246eb086b62520ec304c6759e7ce63e377c7
Joonas Harjukallio <harjukallio@gmail.com>
2021-11-01 14:30:15 +0000

    Comment code

--- src/App.tsx
+++ src/App.tsx
@@ -81,39 +81,39 @@ function App() {
 
 export default App;
 
-interface BaseType {
-  type: string;
-}
-interface Folder extends BaseType {
-  id: number;
-  name: string;
-  parentId: number | null;
-  type: "folder";
-}
-interface Report extends BaseType {
-  id: number;
-  name: string;
-  parentId: number | null;
-  type: "report";
-}
-
-function staticData() {
-  const folders: Folder[] = [
-    { id: 1, name: "Root", parentId: null, type: "folder" },
-    { id: 2, name: "Cats", parentId: 1, type: "folder" },
-    { id: 3, name: "Dogs", parentId: 1, type: "folder" },
-    { id: 4, name: "Small", parentId: 2, type: "folder" },
-    { id: 5, name: "Big", parentId: 2, type: "folder" },
-    { id: 6, name: "Empty folder", parentId: 1, type: "folder" },
-  ];
-
-  const reports: Report[] = [
-    { id: 10, name: "Kitten", parentId: 4, type: "report" },
-    { id: 20, name: "Kitten2", parentId: 4, type: "report" },
-    { id: 30, name: "Buldog", parentId: 3, type: "report" },
-    { id: 40, name: "Under root", parentId: 1, type: "report" },
-    { id: 50, name: "Fatcat", parentId: 5, type: "report" },
-  ];
-
-  return [...folders, ...reports];
-}
+// interface BaseType {
+//   type: string;
+// }
+// interface Folder extends BaseType {
+//   id: number;
+//   name: string;
+//   parentId: number | null;
+//   type: "folder";
+// }
+// interface Report extends BaseType {
+//   id: number;
+//   name: string;
+//   parentId: number | null;
+//   type: "report";
+// }
+//
+// function staticData() {
+//   const folders: Folder[] = [
+//     { id: 1, name: "Root", parentId: null, type: "folder" },
+//     { id: 2, name: "Cats", parentId: 1, type: "folder" },
+//     { id: 3, name: "Dogs", parentId: 1, type: "folder" },
+//     { id: 4, name: "Small", parentId: 2, type: "folder" },
+//     { id: 5, name: "Big", parentId: 2, type: "folder" },
+//     { id: 6, name: "Empty folder", parentId: 1, type: "folder" },
+//   ];
+//
+//   const reports: Report[] = [
+//     { id: 10, name: "Kitten", parentId: 4, type: "report" },
+//     { id: 20, name: "Kitten2", parentId: 4, type: "report" },
+//     { id: 30, name: "Buldog", parentId: 3, type: "report" },
+//     { id: 40, name: "Under root", parentId: 1, type: "report" },
+//     { id: 50, name: "Fatcat", parentId: 5, type: "report" },
+//   ];
+//
+//   return [...folders, ...reports];
+// }
